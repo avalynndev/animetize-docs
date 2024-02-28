41535407f31327335320708a7db60c70c8fc2cff
Avalynn <100826194+avalynndev@users.noreply.github.com>
2024-02-28 14:46:09 +0000

    Updated Animetize Docs to date

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,17 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Animetize | Docs</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/animetize",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/animetize/animetize-docs",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "© 2024-2025 Animetize.",
   },
-}
+};
 
 export default config
