ba540c656cae5e23b7d6534dff5f4d2359fdb32a
Avalynn <100826194+avalynndev@users.noreply.github.com>
2024-04-23 05:30:36 +0000

    Update animetize docs for the upcomming update which supports ONLY gogoanime

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Animetize | Docs</span>,
+  logo: <span>Animetize Documentaion</span>,
+  useNextSeoProps: () => {
+    return { titleTemplate: `%s | Animetize` };
+  },
+  faviconGlyph: "🈸",
   project: {
     link: "https://github.com/animetize",
   },
@@ -12,6 +16,17 @@ const config: DocsThemeConfig = {
   docsRepositoryBase: "https://github.com/animetize/animetize-docs/blob/main",
   footer: {
     text: "© 2024-2025 Animetize.",
+  },
+  head: function useHead() {
+    const description =
+      "The documentation for gogoanime provider on the animetize-api. Find out how to efficiently use the Animetize API here!";
+    return (
+      <>
+        <meta httpEquiv="Content-Language" content="en" />
+        <meta name="description" content={description} />
+        <meta name="og:description" content={description} />
+      </>
+    );
   },
 };
 
